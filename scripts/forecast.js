703fcb229c315f1b69bf65ea1119b1030656b083
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-01-23 19:11:33 +0100

    added first API request

---
+++ scripts/forecast.js
@@ -0,0 +1,19 @@
+const key = '5xehFFJmkbEfU0KsyFUqiTWsrbGtP4rw'
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+
+
+};
+
+getCity('Manchester')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
