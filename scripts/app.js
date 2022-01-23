bfb56640f2c8281dd4dc14af47e9e85d5e8d448f
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-01-23 20:07:25 +0100

    updated UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,29 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    //remove d-none class if present
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+};
 
 const updateCity = async (city) => {
 
@@ -18,6 +43,7 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // update the ui with new city
-    updateCity(city).then(data => console.log(data))
+    updateCity(city)
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
