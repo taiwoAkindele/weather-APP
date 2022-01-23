01398ea61c5abaff7d32aa05b9edc69c8326f84b
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-01-23 20:35:50 +0100

    added images and icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
     //destructure properties
@@ -16,6 +18,18 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    // update night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc)
+
+    let timeSrc = null;
+    if(weather.isDayTime){
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg'
+    }
+    time.setAttribute('src', timeSrc);
 
     //remove d-none class if present
 
